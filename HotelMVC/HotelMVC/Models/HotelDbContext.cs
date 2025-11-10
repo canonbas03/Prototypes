bdc40e0207ac96097baf03dbf0bc2bb9056b7fb3
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-11-10 19:26:04 +0200

    initial CRUD

---
+++ HotelMVC/HotelMVC/Models/HotelDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelMVC.Models
+{
+    public class HotelDbContext : DbContext
+    {
+        public HotelDbContext(DbContextOptions<HotelDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Room> Rooms { get; set; }
+    }
+}
---
+++ HotelMVC/HotelMVC/Models/Room.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelMVC.Models
+{
+    public class Room
+    {
+        public int ID { get; set; }
+        public string? Type { get; set; }
+        public int Number { get; set; }
+
+        public double Price { get; set; }
+    }
+}
