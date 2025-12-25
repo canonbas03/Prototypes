2d9511b3b57fa1cc7c41cac9d8a1643a3218aa47
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-12-25 20:24:07 +0200

    Upgraded order UI

---
+++ HotelMVCPrototype/HotelMVCPrototype/EnumExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+public static class EnumExtensions
+{
+    public static string GetDisplayName(this Enum value)
+    {
+        var field = value.GetType().GetField(value.ToString());
+        var attr = field?.GetCustomAttribute<DisplayAttribute>();
+        return attr?.Name ?? value.ToString();
+    }
+}
--- HotelMVCPrototype/HotelMVCPrototype/Models/Enums/MenuCategory.cs
+++ HotelMVCPrototype/HotelMVCPrototype/Models/Enums/MenuCategory.cs
@@ -1,6 +1,10 @@
-﻿public enum MenuCategory
+﻿using System.ComponentModel.DataAnnotations;
+
+public enum MenuCategory
 {
     Food = 1,
+    [Display(Name = "Alcoholic Beverages")]
     AlcoholicBeverage = 2,
+    [Display(Name = "Non-Alcoholic Beverages")]
     NonAlcoholicBeverage = 3
 }
