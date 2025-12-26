1b1cadd6bde47e9b74f4c3ef30d6934278ffcb08
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2025-12-26 14:10:01 +0200

    Update CreateStayViewModel.cs

--- HotelMVCPrototype/HotelMVCPrototype/Models/CreateStayViewModel.cs
+++ HotelMVCPrototype/HotelMVCPrototype/Models/CreateStayViewModel.cs
@@ -1,9 +1,18 @@
-﻿public class CreateStayViewModel
+﻿using System.ComponentModel.DataAnnotations;
+
+public class CreateStayViewModel
 {
     public int RoomId { get; set; }
 
-    public DateTime CheckInDate { get; set; }
-    public DateTime CheckOutDate { get; set; }
+    [Required]
+    [DataType(DataType.Date)]
+    [Display(Name = "Arrival Date")]
+    public DateTime CheckInDate { get; set; } = DateTime.Today;
+
+    [Required]
+    [DataType(DataType.Date)]
+    [Display(Name = "Departure Date")]
+    public DateTime CheckOutDate { get; set; } = DateTime.Today.AddDays(1);
 
     public List<GuestInputViewModel> Guests { get; set; } = new();
 }
