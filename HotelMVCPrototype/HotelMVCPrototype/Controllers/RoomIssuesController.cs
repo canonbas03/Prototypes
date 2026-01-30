abeca06d2fcfca2832dc8a2dfc3167b2526ecc85
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2026-01-30 21:57:18 +0200

    Not logged in are authorized

--- HotelMVCPrototype/HotelMVCPrototype/Controllers/RoomIssuesController.cs
+++ HotelMVCPrototype/HotelMVCPrototype/Controllers/RoomIssuesController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
-[Authorize]
+
 public class RoomIssuesController : Controller
 {
     private readonly ApplicationDbContext _context;
